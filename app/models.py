751b269e4b7b2b5e5f6ac54f80ff62f7a0dd35bc
MuneerKhaled <muneerkhaled5@gmail.com>
2026-09-25 09:42:59 +0530

    update

--- app/models.py
+++ app/models.py
@@ -3,17 +3,17 @@ from sqlalchemy import Column, Integer, String, Float
 from .database import Base
 
 
-class Job(Base):
-    """Model for storing job information."""
+class Product(Base):
+    """Model for storing product information."""
 
-    __tablename__ = "jobs"
+    __tablename__ = "products"
 
     id = Column(Integer, primary_key=True, index=True)
 
-    title = Column(String, nullable=False)
+    name = Column(String, nullable=False)
 
     description = Column(String, nullable=False)
 
-    salary = Column(Float, nullable=False)
+    price = Column(Float, nullable=False)
 
-    vacancies = Column(Integer, nullable=False, default=1)
+    stock = Column(Integer, nullable=False, default=0)
